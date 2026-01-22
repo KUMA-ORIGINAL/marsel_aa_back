9e41c26e37b9bb3bc6a62385684b44371a06f3bf
KUMA-ORIGINAL <asanovkurmanbek342@gmail.com>
2026-01-23 05:35:51 +0600

    init

--- nginx/nginx_entrypoint.sh
+++ nginx/nginx_entrypoint.sh
@@ -3,11 +3,11 @@
 get_certs_lower=$(echo "$GET_CERTS" | tr '[:upper:]' '[:lower:]')
 
 if [ "$get_certs_lower" = "true" ]; then
-    echo "🆕 Получаем или обновляем сертификаты для доменов: $DOMAIN_GULBOX $DOMAIN_MOBIBO $DOMAIN_MANAS $DOMAIN_JETIGEN..."
+    echo "🆕 Получаем или обновляем сертификаты для доменов: $DOMAIN_MARSEL $DOMAIN_ASIYA $DOMAIN_AMANTUR..."
 
     # собираем список аргументов -d
     domains_args=""
-    for domain in $DOMAIN_GULBOX $DOMAIN_MOBIBO $DOMAIN_MANAS $DOMAIN_JETIGEN; do
+    for domain in $DOMAIN_MARSEL $DOMAIN_ASIYA $DOMAIN_AMANTUR; do
         domains_args="$domains_args -d $domain"
     done
 
